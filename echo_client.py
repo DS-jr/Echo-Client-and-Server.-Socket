bce06c60665fc638ccc0359f5040a2ab4e264d83
DS-jr <dmitry.k.sokolov@gmail.com>
2022-08-18 16:53:10 +0300

    added-main-function-to-Client

--- echo_client.py
+++ echo_client.py
@@ -3,10 +3,16 @@ import socket
 HOST = "127.0.0.1"  # Server's hostname or IP address
 PORT = 5432  # Port used by the server
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:  # AF_INET is the Internet address family for IPv4. SOCK_STREAM is the socket type for TCP
-    s.connect((HOST, PORT))
-    while True:
-        input_in_bytes = str.encode(input('Type some word & press "Enter" (to exit - press "Ctr+C"): '))
-        s.sendall(input_in_bytes)
-        data = s.recv(1024)
-        print(f"Client received back from Server: {data}")
+
+def main():
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:  # AF_INET is the Internet address family for IPv4. SOCK_STREAM is the socket type for TCP
+        s.connect((HOST, PORT))
+        while True:
+            input_in_bytes = str.encode(input('Type some word & press "Enter" (to exit - press "Ctr+C"): '))
+            s.sendall(input_in_bytes)
+            data = s.recv(1024)
+            print(f"Client received back from Server: {data}")
+
+
+if __name__ == '__main__':
+    main()
